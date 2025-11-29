f8c2bd202ccca1405d11cc1d897046395e8d1a7f
chandru <kumaravelchandru982@gmail.com>
2025-11-29 13:43:17 +0530

    Saving the code for azure version

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -13,3 +13,11 @@ output "aks_name" {
 output "acr_login_server" {
   value = azurerm_container_registry.acr.login_server
 }
+
+output "acr_name" {
+  value = azurerm_container_registry.acr.name
+}
+
+output "acr_login_server" {
+  value = azurerm_container_registry.acr.login_server
+}
