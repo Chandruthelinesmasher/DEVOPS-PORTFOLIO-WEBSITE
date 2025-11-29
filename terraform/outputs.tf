dea44c40acf47934e67d8b05a3719038ad078286
chandru <kumaravelchandru982@gmail.com>
2025-11-29 17:50:47 +0530

    Saving the code for azure version

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,19 +1,15 @@
 output "acr_name" {
-  description = "ACR Name"
-  value       = azurerm_container_registry.acr.name
+  value = azurerm_container_registry.acr.name
 }
 
 output "acr_login_server" {
-  description = "ACR Login Server"
-  value       = azurerm_container_registry.acr.login_server
+  value = azurerm_container_registry.acr.login_server
 }
 
 output "aks_cluster_name" {
-  description = "AKS Cluster Name"
-  value       = azurerm_kubernetes_cluster.aks.name
+  value = azurerm_kubernetes_cluster.aks.name
 }
 
 output "aks_rg" {
-  description = "AKS Resource Group"
-  value       = azurerm_resource_group.rg.name
+  value = azurerm_resource_group.rg.name
 }
