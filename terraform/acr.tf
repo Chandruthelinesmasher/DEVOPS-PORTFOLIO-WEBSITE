f6ce4ce05be77f061e14e5a1733de619dc96ea1d
chandru <kumaravelchandru982@gmail.com>
2025-11-29 20:29:02 +0530

    Saving the code for azure version

--- terraform/acr.tf
+++ terraform/acr.tf
@@ -1,7 +1,12 @@
 resource "azurerm_container_registry" "acr" {
-  name                = "${var.project_name}acr"
+  name                = "${lower(var.project_name)}acr${random_integer.acr_rand.result}"
   resource_group_name = azurerm_resource_group.rg.name
   location            = var.location
   sku                 = "Standard"
   admin_enabled       = true
+}
+
+resource "random_integer" "acr_rand" {
+  min = 10000
+  max = 99999
 }
