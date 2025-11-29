3ac1595aee957104feccc083968f82de417293a1
chandru <kumaravelchandru982@gmail.com>
2025-11-29 15:26:15 +0530

    Saving the code for azure version

--- terraform/aks.tf
+++ terraform/aks.tf
@@ -1,22 +1,25 @@
 resource "azurerm_kubernetes_cluster" "aks" {
   name                = "${var.project_name}-aks"
-  location            = var.location
+  location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-  dns_prefix          = "${var.project_name}"
+
+  dns_prefix = "${var.project_name}-dns"
 
   default_node_pool {
-    name                = "system"
-    node_count          = 2
-    vm_size             = "Standard_DS2_v2"
+    name                = "default"
+    node_count          = 1
+    vm_size             = "Standard_DC2s_v3"   # ✅ allowed
+    type                = "VirtualMachineScaleSets"
     vnet_subnet_id      = azurerm_subnet.private[0].id
+    orchestrator_version = "1.29.0"
   }
 
   identity {
     type = "SystemAssigned"
   }
 
-  tags = {
-    Environment = var.environment
-    Project     = var.project_name
+  network_profile {
+    network_plugin = "azure"
+    network_policy = "azure"
   }
 }
