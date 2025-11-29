85f8770e6377264e6071825c9c7963f54c76f243
chandru <kumaravelchandru982@gmail.com>
2025-11-29 18:36:58 +0530

    Saving the code for azure version

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -11,3 +11,10 @@ variable "location" {
   type    = string
   default = "eastus"
 }
+variable "project_name" {
+  type = string
+  validation {
+    condition     = length(var.project_name) >= 3
+    error_message = "project_name must be at least 3 characters."
+  }
+}
