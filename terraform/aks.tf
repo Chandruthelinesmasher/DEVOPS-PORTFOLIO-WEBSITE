72b7a94b1ce181329a1a1c31aa6c369e66081654
chandru <kumaravelchandru982@gmail.com>
2025-11-30 11:18:06 +0530

    Saving the code for azure version

--- terraform/aks.tf
+++ terraform/aks.tf
@@ -24,3 +24,11 @@ resource "azurerm_kubernetes_cluster" "aks" {
     dns_service_ip     = "10.100.0.10"
   }
 }
+
+# CRITICAL: Grant AKS permission to pull from ACR
+resource "azurerm_role_assignment" "aks_acr_pull" {
+  principal_id                     = azurerm_kubernetes_cluster.aks.kubelet_identity[0].object_id
+  role_definition_name             = "AcrPull"
+  scope                            = azurerm_container_registry.acr.id
+  skip_service_principal_aad_check = true
+}
