c8915d91d7826d964f923cb74cdda5109ee87b11
chandru <kumaravelchandru982@gmail.com>
2025-11-30 08:32:11 +0530

    Saving the code for azure version

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,4 +1,16 @@
+locals {
+  project_clean = lower(
+    replace(
+      replace(
+        replace(
+          replace(var.project_name, " ", "-"),
+        "_", "-"),
+      "/", "-"),
+    "-")
+  )
+}
+
 resource "azurerm_resource_group" "rg" {
-  name     = "${var.project_name}-rg"
+  name     = "${local.project_clean}-rg"
   location = var.location
 }
