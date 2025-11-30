40c2c2ed8bec11cf7c2df70fe53be9f6ee9aff4a
chandru <kumaravelchandru982@gmail.com>
2025-11-30 08:47:40 +0530

    Saving the code for azure version

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,16 +1,9 @@
 locals {
-  # Step-by-step sanitization
-  project_clean_step1 = replace(var.project_name, " ", "-")
-  project_clean_step2 = replace(local.project_clean_step1, "_", "-")
-  project_clean_step3 = replace(local.project_clean_step2, "/", "-")
-
-  # Final lowercase sanitized name
-  project_clean = lower(local.project_clean_step3)
+  # Remove all characters that aren't alphanumeric, dash, underscore, parentheses, or period
+  project_clean = lower(replace(var.project_name, "/[^a-zA-Z0-9-_().]/", "-"))
 }
-
 
 resource "azurerm_resource_group" "rg" {
   name     = "${local.project_clean}-rg"
   location = var.location
 }
-
