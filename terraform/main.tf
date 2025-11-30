2ac1e6f9c62bfb86d2a1772cd1330c21a474592f
chandru <kumaravelchandru982@gmail.com>
2025-11-30 08:39:17 +0530

    Saving the code for azure version

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,16 +1,16 @@
 locals {
-  project_clean = lower(
-    replace(
-      replace(
-        replace(
-          replace(var.project_name, " ", "-"),
-        "_", "-"),
-      "/", "-"),
-    "-")
-  )
+  # Step-by-step sanitization
+  project_clean_step1 = replace(var.project_name, " ", "-")
+  project_clean_step2 = replace(local.project_clean_step1, "_", "-")
+  project_clean_step3 = replace(local.project_clean_step2, "/", "-")
+
+  # Final lowercase sanitized name
+  project_clean = lower(local.project_clean_step3)
 }
+
 
 resource "azurerm_resource_group" "rg" {
   name     = "${local.project_clean}-rg"
   location = var.location
 }
+
